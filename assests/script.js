a66f58711596635a593adadfdfb53925b8f95a9c
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2024-08-23 11:36:57 +0530

    portfolio website

---
+++ assests/script.js
@@ -0,0 +1,8 @@
+$(window).on("scroll",function(){
+    var scrollTop= $(window).scrollTop();
+    if(scrollTop>=80){
+      $('body').addClass('fixed-header');
+    }else{
+        $('body').removeClass('fixed-header');
+    }
+})
