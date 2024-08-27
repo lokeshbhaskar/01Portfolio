461a83f6b6bc590130d4a84b86387b4e4f3a345a
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2024-08-28 02:47:35 +0530

    added section

--- assests/script.js
+++ assests/script.js
@@ -1,3 +1,4 @@
+//windows scroll
 $(window).on("scroll",function(){
     var scrollTop= $(window).scrollTop();
     if(scrollTop>=80){
@@ -5,4 +6,14 @@ $(window).on("scroll",function(){
     }else{
         $('body').removeClass('fixed-header');
     }
+})
+//document ready
+
+$(window).ready(function(){
+  //typing animation
+  new Typed('#type-it', {
+    strings: ['Designer', 'Developer'],
+    typeSpeed: 100,
+    loop:true,
+  });
 })
